697ed0bbb130dfea279ac782b68a23ce6b9fa68d
YuxianDeng <yuxian312@gmail.com>
2018-05-16 14:31:00 -0600

    init 29

--- Leetcode/28strStr/28strStr.cpp
+++ Leetcode/28strStr/28strStr.cpp
@@ -1,7 +1,17 @@
 #include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-	return 0;
+	string haystatck = "aaaa", needle = "bba";
+
+	if (needle == "")
+		return 0;
+
+	size_t index =haystatck.find(needle);
+
+	if (index == string::npos)
+		return -1;
+	return index;
 }
